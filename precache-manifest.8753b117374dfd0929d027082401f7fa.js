46665c662c696750aacc9aa1f4b7f553bcb43c2c
Dyanna Tolman <dyannast@gmail.com>
2019-11-18 13:37:00 -0800

    Updates

---
+++ precache-manifest.8753b117374dfd0929d027082401f7fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007ae67f9cd9a380804b51882c5e4e43",
+    "url": "/deetolman/workPortfolio/index.html"
+  },
+  {
+    "revision": "46db245554420cb83403",
+    "url": "/deetolman/workPortfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c38f373832701dde39dc",
+    "url": "/deetolman/workPortfolio/static/js/2.6bc3001f.chunk.js"
+  },
+  {
+    "revision": "46db245554420cb83403",
+    "url": "/deetolman/workPortfolio/static/js/main.faec57d4.chunk.js"
+  },
+  {
+    "revision": "96534f05c6c7cb4664c2",
+    "url": "/deetolman/workPortfolio/static/js/runtime-main.29af9578.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/deetolman/workPortfolio/static/media/logo.25bf045c.svg"
+  }
+]);
